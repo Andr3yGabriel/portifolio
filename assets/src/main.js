5f5d61bf0653fb5451e119b7d9b879a68c13e198
Andr3yGabriel <andreygoncalvesapple@gmail.com>
2024-09-07 04:21:04 -0300

    Feat: Adição da atualização dinâmica das Soft Skills

--- assets/src/main.js
+++ assets/src/main.js
@@ -22,9 +22,16 @@ function updateProfileInfo(profileData) {
     email.href = `mailto:${profileData.email}`
 }
 
+function updateSoftSkill(profileData) {
+    const softSkills = document.getElementById('profile.skills.softskills');
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+}
 
 (async () => {
 
     const profileData = await fetchProfileData();
     updateProfileInfo(profileData);
+    updateSoftSkill(profileData);
+    
 })()
