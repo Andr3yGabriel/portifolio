39f16c45263d5fb60c9cca544535dd84ebd051a2
Andr3yGabriel <andreygoncalvesapple@gmail.com>
2024-09-07 03:42:05 -0300

    Fix: concertando consumo da url

--- assets/src/api.js
+++ assets/src/api.js
@@ -2,7 +2,7 @@
 
 async function fetchProfileData() {
     const url = 'https://github.com/Andr3yGabriel/portifolio/blob/main/data/profile.json';
-    const fetching = fetch(url);
+    const fetching = await fetch(url);
     return await fetching.json();
 }
 
