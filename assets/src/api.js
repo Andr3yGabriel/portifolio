c3b1b1e03bb1315626ce8631a357aaa21c192e85
Andr3yGabriel <andreygoncalvesapple@gmail.com>
2024-09-07 03:40:32 -0300

    Fix: Corrigindo url do profile.json

--- assets/src/api.js
+++ assets/src/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData() {
-    const url = 'https://github.com/Andr3yGabriel/portifolio/main/data/profile.json';
+    const url = 'https://github.com/Andr3yGabriel/portifolio/blob/main/data/profile.json';
     const fetching = fetch(url);
     return await fetching.json();
 }
